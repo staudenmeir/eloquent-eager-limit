772e1ac33bcf75867bc0781a9a3001a200a4bae9
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2021-11-16 21:28:00 +0100

    Refactor code coverage upload

--- tests/coverage/scrutinizer.php
+++ tests/coverage/scrutinizer.php
@@ -1,27 +1,32 @@
 <?php
 
-use GuzzleHttp\Client;
+$url = 'https://scrutinizer-ci.com/api/repositories/g/staudenmeir/eloquent-eager-limit/data/code-coverage';
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+$data = [
+    'revision' => exec('git rev-parse HEAD'),
+    'parents' => [
+        0 => exec('git log --pretty=%P -n1 HEAD'),
+    ],
+    'coverage' => [
+        'format' => 'php-clover',
+        'data' => base64_encode(
+            str_replace(
+                getcwd() . '/',
+                '{scrutinizer_project_base_path}/',
+                file_get_contents('coverage.xml')
+            )
+        ),
+    ],
+];
 
-(new Client())->post(
-    'https://scrutinizer-ci.com/api/repositories/g/staudenmeir/eloquent-eager-limit/data/code-coverage',
-    [
-        'json' => [
-            'revision' => exec('git rev-parse HEAD'),
-            'parents' => [
-                0 => exec('git log --pretty=%P -n1 HEAD'),
-            ],
-            'coverage' => [
-                'format' => 'php-clover',
-                'data' => base64_encode(
-                    str_replace(
-                        getcwd() . '/',
-                        '{scrutinizer_project_base_path}/',
-                        file_get_contents('coverage.xml')
-                    )
-                ),
-            ],
-        ],
-    ]
-);
+$options = [
+    'http' => [
+        'method' => 'POST',
+        'header' => 'Content-Type: application/json',
+        'content' => json_encode($data),
+    ],
+];
+
+$context = stream_context_create($options);
+
+file_get_contents($url, false, $context);
